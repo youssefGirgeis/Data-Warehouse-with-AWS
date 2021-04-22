fdea9b402c961cc577e79cb5db0be642246bbfaa
Girgeis <youssef.girgeis@ontario.ca>
2021-04-21 20:33:23 -0400

    Dropping tables

--- sql_queries.py
+++ sql_queries.py
@@ -7,12 +7,13 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
+songplay_table_drop = "DROP TABLE IF EXITS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXITS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
